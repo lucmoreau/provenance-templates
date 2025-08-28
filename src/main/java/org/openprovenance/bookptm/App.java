78413e5d6efe4ae347f2f867166816b1f19452ba
Luc Moreau <luc.moreau@kcl.ac.uk>
2025-08-28 11:38:15 +0100

    reviewed chapter 4

---
+++ src/main/java/org/openprovenance/bookptm/App.java
@@ -0,0 +1,13 @@
+package org.openprovenance.bookptm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
